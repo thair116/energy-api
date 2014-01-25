2aa120ab4835778b76f6c1e091aa451f80941dc8
Travis Hairfield <travis.hairfield@opower.com>
2014-01-25 02:36:52 -0500

    Get the fucking 03_expert working. Must have redis-server, elasticsearch, and mysql servers running

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,56 @@
+require 'zlib'
+require 'yaml'
 
-contents = [
-'Lorem ipsum dolor sit amet.',
-'Consectetur adipisicing elit, sed do eiusmod tempor incididunt.',
-'Labore et dolore magna aliqua.',
-'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-'Excepteur sint occaecat cupidatat non proident.'
-]
-
-puts "Deleting all articles..."
-Article.delete_all
-
-unless ENV['COUNT']
-
-  puts "Creating articles..."
-  %w[ One Two Three Four Five ].each_with_index do |title, i|
-    Article.create title: title, content: contents[i], published_on: i.days.ago.utc
-  end
-
-else
-
-  print "Generating articles..."
-  (1..ENV['COUNT'].to_i).each_with_index do |title, i|
-    Article.create title: "Title #{title}", content: 'Lorem ipsum dolor', published_on: i.days.ago.utc
-    print '.' if i % ENV['COUNT'].to_i/10 == 0
-  end
-  puts "\n"
-
+Zlib::GzipReader.open(File.expand_path('../articles.yml.gz', __FILE__)) do |gzip|
+  puts "Reading articles from gzipped YAML..."
+  @documents = YAML.load_documents(gzip.read)
 end
+
+# Truncate the default ActiveRecord logger output
+ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDERR)
+ActiveRecord::Base.logger.instance_eval do
+  @formatter = lambda do |s, d, p, message|
+    message
+      .gsub(/\[("content", ".*?")\]/m) { |match| match[0..100] + '..."]' }
+      .gsub(/\[("body", ".*?")\]/m   ) { |match| match[0..100] + '..."]' }
+      .strip
+      .concat("\n")
+  end
+end
+
+# Reduce verbosity and truncate the request body of Elasticsearch logger
+Article.__elasticsearch__.client.transport.tracer.level = Logger::INFO
+Article.__elasticsearch__.client.transport.tracer.formatter = lambda do |s, d, p, message|
+  "\n\n" + (message.size > 105 ? message[0..105].concat("...}'") : message) + "\n\n"
+end
+
+# Skip callbacks
+%w| _touch_callbacks
+    _commit_callbacks
+    after_add_for_categories
+    after_add_for_authorships
+    after_add_for_authors
+    after_add_for_comments  |.each do |c|
+    Article.class.__send__ :define_method, c do; []; end
+  end
+
+@documents.each do |document|
+  article = Article.create! document.slice(:title, :abstract, :content, :url, :shares, :published_on)
+
+  article.categories = document[:categories].map do |d|
+    Category.find_or_create_by! title: d
+  end
+
+  article.authors = document[:authors].map do |d|
+    first_name, last_name = d.split(' ').compact.map(&:strip)
+    Author.find_or_create_by! first_name: first_name, last_name: last_name
+  end
+
+  document[:comments].each { |d| article.comments.create! d }
+
+  article.save!
+end
+
+# Import records into the Elasticsearch index
+#
+Article.import batch_size: 100, force: true, refresh: true
