f97d6cefb8f907fa65495d215f27952ef43b34ba
Travis Hairfield <travis.hairfield@opower.com>
2014-01-24 23:37:31 -0500

    Generated Category, Author and Comment resources

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,28 +5,5 @@ class Article < ActiveRecord::Base
   has_many                :authors, through: :authorships
   has_many                :comments
 
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-
-  def self.search(query)
-    __elasticsearch__.search(
-      {
-        query: {
-          multi_match: {
-            query: query,
-            fields: ['title^10', 'content']
-          }
-        },
-        highlight: {
-          pre_tags: ['<em class="label label-highlight">'],
-          post_tags: ['</em>'],
-          fields: {
-            title:   { number_of_fragments: 0 },
-            content: { fragment_size: 25 }
-          }
-        }
-      }
-    )
-  end
-
+  include Searchable
 end
