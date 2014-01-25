aa6cf446ab7437d8aadc9b70f9cdfd500173630c
Travis Hairfield <travis.hairfield@opower.com>
2014-01-24 23:33:41 -0500

    Added the generated Article resource

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :content, :published_on, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 EnergyApi::Application.routes.draw do
+  root to: 'articles#index'
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
