a4bdba4faf64be441710583c3f8c4b70b38cc8c5
Travis Hairfield <travis.hairfield@opower.com>
2014-01-24 23:33:29 -0500

    Initial commit: Clean application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnergyApi::Application.config.secret_key_base = 'ec7a6872c17dd154ebf2b488792b8f01f6156ac9742d02c961acd03cefc6cbb7199303481ab773f51e3b994ab138b8fc3bc24761665775c6ceb392ef856e94d8'
