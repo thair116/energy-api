3c215a7fc1603130c0efb6d08abf2f4f3ea7261f
Travis Hairfield <twh116@gmail.com>
2015-01-20 10:10:50 -0800

    More cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,19 @@ require 'zlib'
 require 'yaml'
 require 'json'
 
-# Zlib::GzipReader.open(File.expand_path('../articles.yml.gz', __FILE__)) do |gzip|
-#   puts "Reading articles from gzipped YAML..."
-#   @documents = YAML.load_documents(gzip.read)
+# def load_line(line)
+#     series = Series.create! line.except("data", "iso3166")
+#     series.save!
 # end
-
-def load_line(line)
-    series = Series.create! line.except("data", "iso3166")
-    series.save!
-end
-#
-# # we already do this manually by loading eia.sql
-# # or we could load ELEC.txt here
-# puts "Reading articles from sample ELEC file"
-# File.open(File.expand_path('../../ELEC_sample.json', __FILE__)).each_line do |line|
-#   # @documents = JSON.load(file.read)
-#   line = JSON.parse(line)
-#   load_line(line)
-# end
+# #
+# # # we already do this manually by loading eia.sql
+# # # or we could load ELEC.txt here
+# # puts "Reading articles from sample ELEC file"
+# # File.open(File.expand_path('../../ELEC_sample.json', __FILE__)).each_line do |line|
+# #   # @documents = JSON.load(file.read)
+# #   line = JSON.parse(line)
+# #   load_line(line)
+# # end
 
 # Truncate the default ActiveRecord logger output
 ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDERR)
@@ -38,35 +33,6 @@ end
 # Series.__elasticsearch__.client.transport.tracer.formatter = lambda do |s, d, p, message|
 #   "\n\n" + (message.size > 105 ? message[0..105].concat("...}'") : message) + "\n\n"
 # end
-
-# Skip callbacks
-# %w| _touch_callbacks
-#     _commit_callbacks
-#     after_add_for_categories
-#     after_add_for_authorships
-#     after_add_for_authors
-#     after_add_for_comments  |.each do |c|
-#     Article.class.__send__ :define_method, c do; []; end
-#   end
-
-# @documents.each do |document|
-#   series = Series.create! document.slice(:series_id, :name, :units,
-#     :frequency,  :description,  :copyright,  :source,
-#     :iso3116, :start, :end, :last_updated, :lat, :lon)
-
-
-
-
-  # article.categories = document[:categories].map do |d|
-  #   Category.find_or_create_by! title: d
-  # end
-
-  # article.authors = document[:authors].map do |d|
-  #   first_name, last_name = d.split(' ').compact.map(&:strip)
-  #   Author.find_or_create_by! first_name: first_name, last_name: last_name
-  # end
-
-  # document[:comments].each { |d| article.comments.create! d }
 
 # Import records into the Elasticsearch index
 #
