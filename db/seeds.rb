62711398f2b3307f365af6728a2ee1e455794782
Travis Hairfield <twh116@gmail.com>
2015-01-19 20:12:31 -0800

    Bypass dynamic seeding to load manual sql

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,15 @@ def load_line(line)
     series = Series.create! line.except("data", "iso3166")
     series.save!
 end
-
-puts "Reading articles from sample ELEC file"
-File.open(File.expand_path('../../ELEC_sample.json', __FILE__)).each_line do |line|
-  # @documents = JSON.load(file.read)
-  line = JSON.parse(line)
-  load_line(line)
-end
+#
+# # we already do this manually by loading eia.sql
+# # or we could load ELEC.txt here
+# puts "Reading articles from sample ELEC file"
+# File.open(File.expand_path('../../ELEC_sample.json', __FILE__)).each_line do |line|
+#   # @documents = JSON.load(file.read)
+#   line = JSON.parse(line)
+#   load_line(line)
+# end
 
 # Truncate the default ActiveRecord logger output
 ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDERR)
